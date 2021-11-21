be92d25b4571be7f67da3c91255eee156198291c
jiangting <jiang.ting@northeastern.edu>
2021-11-20 16:38:41 -0800

    test two tickets

--- src/tickets/Page.js
+++ src/tickets/Page.js
@@ -30,42 +30,4 @@ const Page = ({data}) =>{
 };
 export default Page;
 
-class Pagination extends React.Component {
-    createPagination = count => {
-        let numPages =
-            Math.floor(count / listingsPerPage) + (count % listingsPerPage > 0 && 1);
-        let arr = [];
-        for (let i = 1; i <= numPages; i++) {
-            arr.push(i);
-        }
-        return arr.map(page => (
-            <div
-                key={`page-${page}`}
-                className="pagination--button"
-                onClick={() => {
-                    this.props.fetchListings(page);
-                }}
-            >
-                {
-                    <span
-                        className={
-                            this.props.currentPage == page
-                                ? "pagination--button__selected"
-                                : "pagination--button__unselected"
-                        }
-                    >
-            {page}
-          </span>
-                }
-            </div>
-        ));
-    };
-    
-    render() {
-        return (
-            <div className="pagination">
-                {this.props.data && this.createPagination(this.props.data.count)}
-            </div>
-        );
-    }
-}
+
