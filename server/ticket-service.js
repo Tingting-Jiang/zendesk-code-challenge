82c0563e888151bcc91414b486ac24d51c3082b3
jiangting <jiang.ting@northeastern.edu>
2021-11-21 10:12:22 -0800

    merge files

--- server/ticket-service.js
+++ server/ticket-service.js
@@ -10,42 +10,6 @@ const AUTH = "Basic " + base.encode(USER + ":" + PASS);
 
 
 module.exports = (app) =>  {
-
-    const fetchTickets = (req, res) => {
-
-       const tickets = "/api/v2/tickets.json";
-
-        fetch(URL + tickets, {
-            method: "GET",
-            headers: {
-                Authorization: AUTH
-            }
-        })
-            .then(response => {
-                if (response.ok) {
-                    // console.log(response);
-                    return response.json();
-                } else {
-                    console.log('!!!---Error---!!!')
-                    throw response;
-                }
-            })
-            .then(ticket => {
-                res.send(ticket);
-                // res.send(res.paginatedResults)
-                console.log('done send data out');
-    
-            })
-            .catch(e => {
-                // console.log('!!!---Error---!!!');
-                // console.log(`Failed to get tickets for page ${req.params.pageNum}.`);
-                // console.log(`[status: ${e.response.status}] [${e.response.statusText}] [${e.response.data.error}]`)
-                res.status(e.status);
-                res.send(e);
-            });
-
-    }
-    
     
     const fetchAllTickets = (req, res) => {
         const tickets = `/api/v2/tickets.json?sort_by=created_at&sort_order=desc&page=${
@@ -69,10 +33,9 @@ module.exports = (app) =>  {
                 }
             })
             .then(ticket => {
-                // console.log(ticket)
+           
                 res.send(ticket);
-                // console.log(tickets);
-                // console.log('done send data out');
+             
                 
             })
             .catch(e => {
@@ -88,34 +51,3 @@ module.exports = (app) =>  {
     
     app.get("/:thisPage/:pageNum", fetchAllTickets);
 }
-
-
-// const paginatedResults =(model) => {
-//
-//     return (req, res, next) => {
-//         const page = parseInt(req.query.page);
-//         const limit = parseInt(req.query.limit);
-//         const startIndex = (page - 1) * limit;
-//         const endIndex = page * limit;
-//
-//         const results = {};
-//         if (endIndex < model.length) {
-//             results.next = {
-//                 page: page + 1,
-//                 limit: limit
-//             }
-//         }
-//
-//         if (startIndex > 0) {
-//             results.previous = {
-//                 page: page - 1,
-//                 limit: limit
-//             }
-//         }
-//         results.results = model.slice(startIndex, endIndex);
-//
-//         res.paginatedResults = results;
-//         next();
-//
-//     }
-// }
--- src/tickets/fetchTickets.js
+++ src/tickets/fetchTickets.js
@@ -1,9 +1,6 @@
-// import URL from "../data/data";
-// import TicketsPerPage from "../data/data";
 
 
 const URL = "http://localhost:4000";
-// const TicketsPerPage = 2;
 
 export const fetchSomeTickets = (TicketsPerPage, page) =>
     
@@ -11,13 +8,5 @@ export const fetchSomeTickets = (TicketsPerPage, page) =>
         .then(console.log("backend localhost: ", `${URL}/${TicketsPerPage}/${page}`))
         .then(response => response.json());
 
-       
-//
-// export const fetchTickets = () =>
-//     fetch(URL)
-//         .then(response => response.json());
 
-
-
-// export default {fetchTickets};
     
